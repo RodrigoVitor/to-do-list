f712efc0e610d40db34c7306d3eeddfe34c6aea5
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-09-06 09:03:16 -0300

    Creating the footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AddTask } from "./components/AddTask"
 import { TaskList } from "./components/TaskList"
 import './App.css'
 import { useTaskList } from "./hooks/TaskList"
+import { Footer } from "./components/Footer"
 
 const App = () => {
   const [taskReducer, dispatch] = useTaskList()
@@ -31,20 +32,19 @@ const App = () => {
   }
 
   return (
-    <div className="p-5">
+    <div className="p-5 relative ">
       <header className="bg-green-300 flex justify-center p-2 mt-3">
         <h1 className="text-3xl font-bold text-[#fff]">Todo List</h1>
       </header>
       
-      <main className="lg:flex lg:justify-between">
+      <main className="lg:flex lg:justify-between min-h-[70vh]">
         <AddTask actionAddTask={handleAddTask} actionRemoveAll={handleRemoveAll} />
         <section className="lg:w-3/6">
           <TaskList tasks={taskReducer} removeTask={handleRemoveTask}/>
         </section>
       </main>
 
-  
-      
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+    return (
+        <footer className="flex justify-center w-11/12 pb-4 mt-4">
+            <p>By - 
+                <a 
+                    href="https://github.com/RodrigoVitor/to-do-list" className="hover:text-[#00a]"
+                    target="_blank"
+                >
+                    Rodrigo Vitor
+                </a>
+            </p>
+        </footer>
+    )
+}
